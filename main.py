40d0090ba32888adc90b4060b525c08bf25779eb
emperorkuzco4k-hash <emperorkuzco4k@gmail.com>
2026-02-18 08:47:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set.")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات با موفقیت روشن شد ✅")
-
-async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("pong 🏓")
+    await update.message.reply_text("ربات با موفقیت اجرا شد ✅")
 
 def main():
-    if not TOKEN:
-        raise RuntimeError("BOT_TOKEN is not set. Add it in Render Environment variables.")
-
-    app = Application.builder().token(TOKEN).build()
-
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("ping", ping))
-
     app.run_polling()
 
 if __name__ == "__main__":
